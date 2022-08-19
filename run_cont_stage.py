71bac435459b606fc61941c5dab2f02d42e5cf14
Prateek Grover <110556096+pgroverccb@users.noreply.github.com>
2022-08-19 19:01:55 +0530

    Update run_cont_stage.py

--- run_cont_stage.py
+++ run_cont_stage.py
@@ -9,6 +9,14 @@ import tifffile
 import json
 import random
 import pickle
+import sys
+import builtins
+import os
+
+sys.stdout = open("/mnt/home/pgrover/continous_cell_cycle_stage_pred/logs/continous_logging_cont_stage.txt", "w", buffering=1)
+def print(text):
+    builtins.print(text)
+    os.fsync(sys.stdout)
 
 class Dataset(torch.utils.data.Dataset):
   def __init__(self, list_IDs):
@@ -24,9 +32,9 @@ class Dataset(torch.utils.data.Dataset):
         sample = pickle.load(file)
         input = sample['input']
         output = sample['output']
-        z_offset = 0
-        y_offset = 0
-        x_offset = 0
+        z_offset = random.randint(5, 25)
+        y_offset = random.randint(50, 70)
+        x_offset = random.randint(90, 110)
         input = input[:, z_offset : z_offset + 128, y_offset : y_offset + 128, x_offset : x_offset + 128]
         output = output[:, z_offset : z_offset + 128, y_offset : y_offset + 128, x_offset : x_offset + 128]
         return input, output
@@ -35,7 +43,7 @@ class Dataset(torch.utils.data.Dataset):
 params = {'batch_size': 2,
           'shuffle': True,
           'num_workers': 0}
-max_epochs = 10
+# max_epochs = 10
 
 # Datasets
 partition = {'train' : [], 'validation' : []}
@@ -55,11 +63,13 @@ validation_generator = torch.utils.data.DataLoader(validation_set, **params)
 
 stage_pred_model = UNet3D(in_channel = 2, out_channel = 2, is_isotropic = True)
 stage_pred_model = stage_pred_model.cuda()
+# stage_pred_model.load_state_dict(torch.load("/mnt/ceph/users/pgrover/saved_model_.pth"))
 
 mse_loss = nn.MSELoss()
-optimizer = torch.optim.Adam(stage_pred_model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(stage_pred_model.parameters(), lr=0.0002)
+sig = nn.Sigmoid()
 print("Begin training.")
-for e in range(1, 10+1):
+for e in range(1, 200+1):
     stage_pred_model.train()
     batch_num = 0
     train_loss_avg = 0.0
@@ -72,8 +82,8 @@ for e in range(1, 10+1):
         y_train_pred = stage_pred_model(X_train_batch)
         l2_growth_pre = mse_loss(y_train_pred[0, 0, :, :, :], y_train_batch[0, 0, :, :, :])
         l2_growth_post = mse_loss(y_train_pred[0, 1, :, :, :], y_train_batch[0, 1, :, :, :])
-        # print("L1 x : ", round(l2_x.item(), 3), "| L1 y: ", round(l2_y.item(), 3), "| L1 z: ", round(l2_z.item(), 3))
-        train_loss = l2_growth_pre + l2_growth_post
+        print("T : L2 Growth Pre : " + str(round(l2_growth_pre.item(), 3)) + "| L2 Growth Post : " + str(round(l2_growth_post.item(), 3)))
+        train_loss = (l2_growth_pre + l2_growth_post)
         train_loss_avg += train_loss.item()
         train_loss.backward()
         optimizer.step()
@@ -85,9 +95,91 @@ for e in range(1, 10+1):
         y_val_pred = stage_pred_model(X_val_batch)
         l2_growth_pre = mse_loss(y_val_pred[0, 0, :, :, :], y_val_batch[0, 0, :, :, :])
         l2_growth_post = mse_loss(y_val_pred[0, 1, :, :, :], y_val_batch[0, 1, :, :, :])
-        # print("L1 x : ", round(l2_x.item(), 3), "| L1 y: ", round(l2_y.item(), 3), "| L1 z: ", round(l2_z.item(), 3))
-        val_loss = l2_growth_pre + l2_growth_post
+        print("V : L2 Growth Pre : " + str(round(l2_growth_pre.item(), 3)) + "| L2 Growth Post : " + str(round(l2_growth_post.item(), 3)))
+        val_loss = (l2_growth_pre + l2_growth_post)
         val_loss_avg += val_loss.item()
         optimizer.step()
-    print("Epoch : ", e, "Train Loss : ", round(train_loss_avg/len(partition['train']), 3), "Val Loss : ", round(val_loss_avg/len(partition['validation']), 3))
-torch.save(stage_pred_model.state_dict(), "/mnt/home/pgrover/continous_cell_cycle_stage_pred/utils/saved_model.pth")
+    print("Epoch : " + str(e) + "Train Loss : " + str(round(train_loss_avg/len(partition['train']), 3)) + "Val Loss : " + str(round(val_loss_avg/len(partition['validation']), 3)))
+    torch.save(stage_pred_model.state_dict(), "/mnt/ceph/users/pgrover/saved_model_" + str(e) + ".pth")
+
+    testing_partition = []
+    for i in range(130, 131):
+        testing_partition.append(i)
+
+    print("Begin Testing.")
+    for ID in testing_partition[:1]:
+        file = open("/mnt/ceph/users/pgrover/growth_field_dataset/sample_" + str(ID) + ".pkl", 'rb')
+        sample_full = pickle.load(file)
+        input = torch.Tensor(sample_full['input'].reshape((1, sample_full['input'].shape[0], sample_full['input'].shape[1], sample_full['input'].shape[2], sample_full['input'].shape[3])))
+        output = torch.Tensor(sample_full['output'].reshape((1, sample_full['output'].shape[0], sample_full['output'].shape[1], sample_full['output'].shape[2], sample_full['output'].shape[3])))
+        input = input[:, :, 13 : 13 + 128, 64 : 64 + 128, 96 : 96 + 128]
+        output = output[:, :, 13 : 13 + 128, 64 : 64 + 128, 96 : 96 + 128]
+        # input, output = input.to('cuda', dtype = torch.float), output.to('cuda', dtype = torch.float)
+        output_pred = stage_pred_model(input.cuda())
+        # np.save("/mnt/home/pgrover/continous_cell_cycle_stage_pred/utils/testing_pred_" + str(ID) + ".npy", output_pred.detach().cpu().numpy())
+
+
+    for index in testing_partition[:1]:
+        print("Index : " + str(index))
+        # output_pred = np.load("/mnt/home/pgrover/continous_cell_cycle_stage_pred/utils/testing_pred_" + str(index) + ".npy")
+        file = open("/mnt/ceph/users/pgrover/growth_field_dataset/sample_" + str(ID) + ".pkl", 'rb')
+        sample_full = pickle.load(file)
+        output = torch.Tensor(sample_full['output'].reshape((1, sample_full['output'].shape[0], sample_full['output'].shape[1], sample_full['output'].shape[2], sample_full['output'].shape[3])))    
+        output = output[:, :, 13 : 13 + 128, 64 : 64 + 128, 96 : 96 + 128]
+        five_digit_str = str(index)
+        while (len(five_digit_str) != 5):
+            five_digit_str = '0' + five_digit_str
+        mask_pre = tifffile.imread("/mnt/ceph/users/pgrover/32_40_dataset/registered_label_images/label_reg_" + str(five_digit_str) + ".tif")
+
+        five_digit_str = str(index + 1)
+        while (len(five_digit_str) != 5):
+            five_digit_str = '0' + five_digit_str
+        mask_post = tifffile.imread("/mnt/ceph/users/pgrover/32_40_dataset/registered_label_images/label_reg_" + str(five_digit_str) + ".tif")
+
+        for curr_index in np.unique(mask_pre)[1:]:
+            curr_volume = np.copy(mask_pre)[13 : 13 + 128, 64 : 64 + 128, 96 : 96 + 128]
+            curr_volume[curr_volume != curr_index] = 0
+            curr_volume[curr_volume == curr_index] = 1
+            all_pixels = len(np.argwhere(curr_volume) == 1)
+            curr_volume = curr_volume * output_pred[0, 0].detach().cpu().numpy()
+            growth_avg = np.sum(curr_volume) * 1.0 / all_pixels
+            growth_avg = min(1.0, growth_avg)
+            growth_avg = max(0.0, growth_avg)
+            print("Growth stage pred in pre frame for " + str(curr_index) + " is : " + str(round(growth_avg, 3)))
+            # print(" ")
+            curr_volume = np.copy(mask_pre)[13 : 13 + 128, 64 : 64 + 128, 96 : 96 + 128]
+            curr_volume[curr_volume != curr_index] = 0
+            curr_volume[curr_volume == curr_index] = 1
+            all_pixels = len(np.argwhere(curr_volume) == 1)
+            curr_volume = curr_volume * output[0, 0].detach().numpy()
+            growth_avg = np.sum(curr_volume) * 1.0 / all_pixels
+            growth_avg = min(1.0, growth_avg)
+            growth_avg = max(0.0, growth_avg)
+            print("Growth stage actual in pre frame for " + str(curr_index) + " is : " + str(round(growth_avg, 3)))
+            print(" ")
+        print(" ")
+        print(" ")
+
+        for curr_index in np.unique(mask_post)[1 : ]:
+            curr_volume = np.copy(mask_post)[13 : 13 + 128, 64 : 64 + 128, 96 : 96 + 128]
+            curr_volume[curr_volume != curr_index] = 0
+            curr_volume[curr_volume == curr_index] = 1
+            all_pixels = len(np.argwhere(curr_volume) == 1)
+            curr_volume = curr_volume * output_pred[0, 1].detach().cpu().numpy()
+            growth_avg = np.sum(curr_volume) * 1.0 / all_pixels
+            growth_avg = min(1.0, growth_avg)
+            growth_avg = max(0.0, growth_avg)
+            print("Growth stage pred in post frame for " + str(curr_index) + " is : " + str(round(growth_avg, 3)))
+            # print(" ")
+            curr_volume = np.copy(mask_post)[13 : 13 + 128, 64 : 64 + 128, 96 : 96 + 128]
+            curr_volume[curr_volume != curr_index] = 0
+            curr_volume[curr_volume == curr_index] = 1
+            all_pixels = len(np.argwhere(curr_volume) == 1)
+            curr_volume = curr_volume * output[0, 1].detach().numpy()
+            growth_avg = np.sum(curr_volume) * 1.0 / all_pixels
+            growth_avg = min(1.0, growth_avg)
+            growth_avg = max(0.0, growth_avg)
+            print("Growth stage actual in post frame for " + str(curr_index) + "is :" + str(round(growth_avg, 3)))
+            print(" ")
+        print(" ")
+        print(" ")
